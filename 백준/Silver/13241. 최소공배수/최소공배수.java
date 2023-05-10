8d7a2bc1a9009b940392fa984c4171e2cf48be28
Hyewon Lyu <52669844+lyuhw1023@users.noreply.github.com>
2023-05-10 17:06:10 +0900

    [Silver V] Title: 최소공배수, Time: 124 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long r = gcd(A, B);
+        long result = A * B / r;
+        System.out.println(result);
+
+    }
+
+    public static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+}
