0dd0325dc743384fb29bbbb82543618d291315ec
Hyewon Lyu <52669844+lyuhw1023@users.noreply.github.com>
2023-05-10 16:04:09 +0900

    [Bronze IV] Title: 기찍 N, Time: 1052 ms, Memory: 29632 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
