ddf37215195c08b6d0bb4bd169252135b08fbd74
Hyewon Lyu <52669844+lyuhw1023@users.noreply.github.com>
2023-06-04 16:27:04 +0900

    [Bronze II] Title: 피보나치 수, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -7,16 +7,16 @@ public class Main {
         int res1 = 0;
         int res2 = 1;
         int res3 = 0;
-        for (int i = 2; i <= n; i++) {
-            res3 = res1 + res2;
-            res1 = res2;
-            res2 = res3;
-        }
         if (n == 0) {
             System.out.println(0);
         } else if (n == 1) {
             System.out.println(1);
         } else {
+            for (int i = 2; i <= n; i++) {
+                res3 = res1 + res2;
+                res1 = res2;
+                res2 = res3;
+            }
             System.out.println(res3);
         }
     }
